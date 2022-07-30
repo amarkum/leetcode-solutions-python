89f531984872fef1b56c25f79d34345f5910a5ee
Amar Kumar <amar.mailsbox@gmail.com>
2022-07-30 18:39:31 +0530

    Added code to sort a dictionary

--- playground.py
+++ playground.py
@@ -1,12 +1,22 @@
-# convert list into string
+# 1. convert list into string
 words = ["l", "e", "e", "t", "c", "o", "d", "e"]
 text = "".join(words)
 print(text)
 
-# get the last or last n-th element from the list
+# 2. get the last or last n-th element from the list
 # expected 2
 nums = [2, 3, 5, 6, 7, 8, 2]
 print(nums[-1])
 
 # last 3rd element
 print(nums[-3])
+
+# 3. Count the element of the list
+numbers = [2, 3, 6, 3, 2, 2]
+duplicate_count = {number: numbers.count(number) for number in numbers}
+print(duplicate_count)
+
+# 4. Sort dictionary by value
+dictionary = {"python": 3, "java": 2, "go": 4}
+sorted_dict = dict(sorted(dictionary.items(), key=lambda e: e[1]))
+print(sorted_dict)
