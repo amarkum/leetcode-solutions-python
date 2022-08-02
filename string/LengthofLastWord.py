c926ce6a25aa3387d6cebfd048f7f666a1f2f1ed
Amar Kumar <amar.mailsbox@gmail.com>
2022-08-02 23:24:12 +0530

    Update Length of Last Word

---
+++ string/LengthofLastWord.py
@@ -0,0 +1,21 @@
+"""
+https://leetcode.com/problems/length-of-last-word/
+
+Given a string s consisting of words and spaces, return the length of the last word in the string.
+A word is a maximal substring consisting of non-space characters only.
+
+Input: s = "Hello World"
+Output: 5
+Explanation: The last word is "World" with length 5.
+"""
+class LengthofLastWord:
+    def lengthOfLastWord(self, s: str) -> int:
+        s = s.strip()
+        parts = s.split(" ")
+        return len(parts[len(parts) - 1].strip())
+
+
+Length_of_last_word = LengthofLastWord()
+s = "   fly me   to   the moon  "
+
+print(Length_of_last_word.lengthOfLastWord(s))
